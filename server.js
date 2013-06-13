6e7fb088047300e505a2ab967a1021ce9065c2fa
givenm <givenmjay@gmail.com>
2013-06-13 20:28:18 +0200

    Changed the server name to server.js from app.js

---
+++ server.js
@@ -0,0 +1,38 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routesConfigs = require('./routes/config/routes-config')
+  , http = require('http')
+  , path = require('path');
+ var engines = require('consolidate');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.engine('.html', require('ejs').__express);
+app.set('view engine', 'html');
+//app.engine('.html', require('ejs').__express);
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+//configure all routes so that they are not done here
+routesConfigs.initAllRoutes(app);
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
